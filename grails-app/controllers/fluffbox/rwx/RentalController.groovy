bdaf385d61df938ee33de844674815b561e0b2ba
Matt Stine <matt.stine@gmail.com>
2012-06-09 23:35:28 -0400

    changes for si

--- grails-app/controllers/fluffbox/rwx/RentalController.groovy
+++ grails-app/controllers/fluffbox/rwx/RentalController.groovy
@@ -1,27 +1,46 @@
 package fluffbox.rwx
 
 import grails.plugins.springsecurity.Secured
+
+import com.fluffbox.si.model.CreditCardAuthorization;
 import com.fluffbox.si.model.Transaction
 
 class RentalController {
 	
 	def gateway
 	def transactionService
+	def creditCardAuthorizationService
 
     def reserve = {
       [clone:SpeakerClone.get(params.id)]    
     }
+	
+	@Secured(['ROLE_CUSTOMER'])
+	def payment = {
+		[denied:params.denied]
+	}
+	
+	def authorizeCreditCard = {
+		CreditCardAuthorization auth = new CreditCardAuthorization(params.cardNumber, params.expDate, 1.07)
+		
+		def result = creditCardAuthorizationService.authorizeTransaction(auth);
+		
+		if (result.equals("OK")) {
+			redirect action: approved	
+		} else {
+			redirect action: payment, params: [denied:true]
+		}
+	}
 
-    @Secured(['ROLE_CUSTOMER'])
-    def payment = {
+    def approved = {
 		def result = gateway.convertToUpperCase("success!")
 		
 		def transaction = new Transaction();
 		
 		transaction.with {
 			customerId = "123456789"
-			accountNumber = "1111222233334444"
-			speakerId = "1"
+			accountNumber = "4242424242424242"
+			speakerId = "Mark Richards"
 			kioskId = "1"
 			date = new Date()
 			amount = 1.07
--- grails-app/views/rental/payment.gsp
+++ grails-app/views/rental/payment.gsp
@@ -5,7 +5,28 @@
 
   </head>
   <body>
-    <h1>This concludes your fake rental experience!</h1>
-    <p>${result}</p>
+    <h1>Enter payment information:</h1>
+    
+    <g:if test="${denied}">
+    <h2><font color="red">Your card was denied. Please try another</font></h2>
+    </g:if>
+    
+    <p>
+    <g:form action="authorizeCreditCard">
+    
+    <p>
+    <label for="cardNumber">Card Number</label><br/>
+    <g:textField id="cardNumber" name="cardNumber"/>
+    </p>
+    
+    <p>
+    <label for="expDate" >Expiration Date</label><br/>
+    <g:textField id="expDate" name="expDate"/>
+    </p>
+    
+    <g:submitButton name="go" value="GO!"/>
+    
+    </g:form>
+    </p>
   </body>
 </html>
